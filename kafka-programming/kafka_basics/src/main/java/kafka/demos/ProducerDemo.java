ffc22b012c0acd26494770da252a06ccded2c4a9
Khoa, Le <leminkhoa@gmail.com>
2023-06-16 21:05:32 +0700

    Completed ConsumerDemo and ConsumerDemowithShutdown

--- kafka-programming/kafka_basics/src/main/java/kafka/demos/ProducerDemo.java
+++ kafka-programming/kafka_basics/src/main/java/kafka/demos/ProducerDemo.java
@@ -14,7 +14,7 @@ public class ProducerDemo {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("hello world");
+        log.info("I am a Kafka Producer");
 
         // create Properties
         Properties properties = new Properties();
