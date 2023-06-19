106c8293585c7f684d0dd3ec2381c18741e89b68
Khoa, Le <leminkhoa@gmail.com>
2023-06-19 15:11:16 +0700

    Completed kafka_producer_wikimedia

--- kafka-programming/kafka_producer_wikimedia/src/main/java/kafka/demos/WikimediaChangesProducer.java
+++ kafka-programming/kafka_producer_wikimedia/src/main/java/kafka/demos/WikimediaChangesProducer.java
@@ -2,6 +2,7 @@ package kafka.demos;
 
 import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 
 public class WikimediaChangesProducer {
@@ -24,12 +26,17 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         
         String topic = "wikimedia.recentchange";
 
-        EventHandler eventHandler = TODO;
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
         EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
         EventSource eventSource = builder.build();
@@ -37,5 +44,11 @@ public class WikimediaChangesProducer {
         // start the producer in another thread
         eventSource.start();
 
+        // we produce for 10 minutes and block the program until then
+        try {
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
